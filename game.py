142fcd7d85bb8801b81ba82a39cffd6f2f8c551b
Aaron Brombergues <abrom010@fiu.edu>
2019-11-05 01:06:59 -0500

    moving files from project folder

---
+++ game.py
@@ -0,0 +1,80 @@
+import pygame
+import sys
+
+from script import *
+
+#initializing the pygame window and game variables
+gameDisplay = pygame.display.set_mode((800, 800))
+pygame.display.set_caption('Checkers')
+red_translucent = pygame.image.load("red_translucent.png")
+black_translucent = pygame.image.load("black_translucent.png")
+clock = pygame.time.Clock()
+
+#main loop
+is_black = True
+player_piece_value = get_player_piece(is_black)
+board = make_board(is_black)
+player_turn = True
+running = True
+moving = False
+indexes = []
+moving_indexes = []
+place_holder = 0
+draw_board(board, gameDisplay)
+pygame.display.flip()
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if (event.type == pygame.MOUSEBUTTONDOWN) and (event.button == 1) and not moving and player_turn:
+            x,y = pygame.mouse.get_pos()
+            indexes = where_clicked(board,x,y)
+            if board[indexes[0]][indexes[1]]==1 and not is_black:
+                place_holder = board[indexes[0]][indexes[1]]
+                board[indexes[0]][indexes[1]]=0
+                draw_board(board, gameDisplay)
+                moving = True
+            if board[indexes[0]][indexes[1]]==2 and is_black:
+                place_holder = board[indexes[0]][indexes[1]]
+                board[indexes[0]][indexes[1]]=0
+                draw_board(board, gameDisplay)
+                moving = True
+
+        elif (event.type == pygame.MOUSEBUTTONDOWN) and (event.button == 1) and moving and player_turn:
+            x,y = pygame.mouse.get_pos()
+            moving_indexes = where_clicked(board,x,y)
+            valid = valid_move(board,indexes,moving_indexes,player_piece_value)
+            if type(valid)!=bool:
+                    board[moving_indexes[0]][moving_indexes[1]]=place_holder
+                    place_holder = 0
+                    jumping_indexes = valid[1]
+                    board[jumping_indexes[0]][jumping_indexes[1]]=0
+                    draw_board(board, gameDisplay)
+                    pygame.display.update()
+                    moving = False
+            else:
+                if valid:
+                    board[moving_indexes[0]][moving_indexes[1]]=place_holder
+                    place_holder = 0
+                    draw_board(board, gameDisplay)
+                    pygame.display.update()
+                    moving = False
+                else:
+                    moving = False
+                    board[indexes[0]][indexes[1]]=place_holder
+                    draw_board(board, gameDisplay)
+                    pygame.display.update()
+
+    if moving == True:
+        x,y = pygame.mouse.get_pos()
+        draw_board(board, gameDisplay)
+        if place_holder == 1: gameDisplay.blit(red_translucent, tuple(map(lambda x: x-50,pygame.mouse.get_pos())))
+        else: gameDisplay.blit(black_translucent, tuple(map(lambda x: x-50,pygame.mouse.get_pos())))
+        clock.tick(60)
+        pygame.display.update()
+#end of main loop
+
+pygame.display.quit()
+pygame.quit()
+sys.exit()
---
+++ script.py
@@ -0,0 +1,89 @@
+#Checkers game
+import pygame
+
+boardImage = pygame.image.load("board.png")
+red = pygame.image.load("red.png")
+black = pygame.image.load("black.png")
+
+coordinates =  (((0, 0), (100, 0), (200, 0), (300, 0), (400, 0), (500, 0), (600, 0), (700, 0)),
+                ((0, 100), (100, 100), (200, 100), (300, 100), (400, 100), (500, 100), (600, 100), (700, 100)),
+                ((0, 200), (100, 200), (200, 200), (300, 200), (400, 200), (500, 200), (600, 200), (700, 200)),
+                ((0, 300), (100, 300), (200, 300), (300, 300), (400, 300), (500, 300), (600, 300), (700, 300)),
+                ((0, 400), (100, 400), (200, 400), (300, 400), (400, 400), (500, 400), (600, 400), (700, 400)),
+                ((0, 500), (100, 500), (200, 500), (300, 500), (400, 500), (500, 500), (600, 500), (700, 500)),
+                ((0, 600), (100, 600), (200, 600), (300, 600), (400, 600), (500, 600), (600, 600), (700, 600)),
+                ((0, 700), (100, 700), (200, 700), (300, 700), (400, 700), (500, 700), (600, 700), (700, 700)))
+
+def get_player_piece(is_black):
+    switcher = {
+        True: 2,
+        False: 1
+    }
+    return switcher.get(is_black)
+
+def make_board(is_black=True):
+    if is_black:
+        return ([0, 1, 0, 1, 0, 1, 0, 1],
+                [1, 0, 1, 0, 1, 0, 1, 0],
+                [0, 1, 0, 1, 0, 1, 0, 1],
+                [0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0],
+                [2, 0, 2, 0, 2, 0, 2, 0],
+                [0, 2, 0, 2, 0, 2, 0, 2],
+                [2, 0, 2, 0, 2, 0, 2, 0])
+    if not is_black:
+        return ([0, 2, 0, 2, 0, 2, 0, 2],
+                [2, 0, 2, 0, 2, 0, 2, 0],
+                [0, 2, 0, 2, 0, 2, 0, 2],
+                [0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0],
+                [1, 0, 1, 0, 1, 0, 1, 0],
+                [0, 1, 0, 1, 0, 1, 0, 1],
+                [1, 0, 1, 0, 1, 0, 1, 0])
+
+
+def draw_board(board, gameDisplay):
+    gameDisplay.blit(boardImage, [0, 0])
+    for i in range(8):
+        for j in range(8):
+            if board[i][j] == 1:
+                gameDisplay.blit(red, coordinates[i][j])
+            elif board[i][j] == 2:
+                gameDisplay.blit(black, coordinates[i][j])
+
+def where_clicked(board, x, y):
+    x = x-(x%100)
+    y = y-(y%100)
+    position = (x,y)
+    for row in coordinates:
+        for entry in row:
+            if entry == position:
+                i=coordinates.index(row)
+                j=row.index(entry)
+    return i,j
+
+def valid_move(board,indexes,moving_indexes,player_piece_value):
+    jumping_indexes = []
+    moving_square_value = board[moving_indexes[0]][moving_indexes[1]]
+    if moving_square_value == 0:
+        x_distance = moving_indexes[1] - indexes[1]
+        y_distance = moving_indexes[0] - indexes[0]
+        if y_distance == -2:
+            if x_distance==2:
+                jumping_indexes = [indexes[0]-1,indexes[1]+1]
+                jumping_square_value = board[jumping_indexes[0]][jumping_indexes[1]]
+            elif x_distance==-2:
+                jumping_indexes = [indexes[0]-1,indexes[1]-1]
+                jumping_square_value = board[jumping_indexes[0]][jumping_indexes[1]]
+            else:
+                return False
+            if jumping_square_value!=0 and\
+               jumping_square_value!=player_piece_value:
+                return (True,jumping_indexes)
+        elif y_distance == -1:
+            if x_distance == 1 or x_distance == -1:
+                return True
+    return False
+
+if __name__ == "__main__":
+    pass
